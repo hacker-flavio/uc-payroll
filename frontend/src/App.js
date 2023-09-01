ba03adc7efb10b5c0ef7851927de511325cb2bd6
hacker-flavio <theflavioherrera@gmail.com>
2023-09-01 16:32:38 -0700

    added code to show data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios.get("/payrolls").then((res) => {
+      // Replace single quotes with double quotes in the response text
+      const responseText = res.data.replace(/'/g, '"');
+      // Parse the JSON string to a JavaScript object
+      const parsedData = JSON.parse(responseText);
+      setData(parsedData);
+    });
+  }, []);
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React App</h1>
+      {data && (
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Year</th>
+              <th>Campus</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Title</th>
+              <th>Salary</th>
+              <th>Total Pay</th>
+              <th>Other Pay</th>
+              <th>Benefits</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.rows.map((row) => (
+              <tr key={row.id}>
+                <td>{row.cell[0]}</td>
+                <td>{row.cell[1]}</td>
+                <td>{row.cell[2]}</td>
+                <td>{row.cell[3]}</td>
+                <td>{row.cell[4]}</td>
+                <td>{row.cell[5]}</td>
+                <td>{row.cell[6]}</td>
+                <td>{row.cell[7]}</td>
+                <td>{row.cell[8]}</td>
+                <td>{row.cell[9]}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const path = require("path");
 const app = express();
 
 const port = process.env.PORT || 4050;
+app.use(express.json());
+
+const axios = require("axios");
+const FormData = require("form-data");
 
 // Serve static files from the "build" directory
 app.use(express.static(path.resolve(__dirname, "./frontend/build")));
@@ -12,8 +16,38 @@ app.use(express.static(path.resolve(__dirname, "./frontend/build")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./frontend/build", "index.html"));
 });
-app.get("/message", (req, res) => {
-  res.send("hello from server");
+app.get("/payrolls", (req, res) => {
+  let data = new FormData();
+  data.append("_search", "false");
+  data.append("nd", "1693609828178");
+  data.append("rows", "20");
+  data.append("page", "1");
+  data.append("sidx", "EAW_GRS_EARN_AMT");
+  data.append("sord", "desc");
+  data.append("year", "2022");
+  data.append("location", "Merced");
+
+  let config = {
+    method: "post",
+    maxBodyLength: Infinity,
+    url: "https://ucannualwage.ucop.edu/wage/search.action",
+    headers: {
+      Cookie: "JSESSIONID=0000H5l-s83LwUsRWFvQTLAqqMj:168lpmkau",
+      ...data.getHeaders(),
+    },
+    data: data,
+  };
+
+  axios
+    .request(config)
+    .then((response) => {
+      console.log(JSON.stringify(response.data));
+      res.send(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.send({ message: "error" });
+    });
 });
 
 const server = http.createServer(app);
