ee830ffaf6ba9bedae81509f28b837e18234679c
hacker-flavio <theflavioherrera@gmail.com>
2023-09-01 01:00:15 -0700

    added server code

--- index..js
+++ index..js
@@ -1,1 +1,23 @@
-console.log("hello world");
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const app = express();
+
+const port = process.env.PORT || 4050;
+
+// Serve static files from the "build" directory
+app.use(express.static(path.resolve(__dirname, "./frontend/build")));
+
+// Handle requests for the root URL
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./frontend/build", "index.html"));
+});
+app.get("/message", (req, res) => {
+  res.send("hello from server");
+});
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
